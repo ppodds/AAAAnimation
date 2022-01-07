a3494ac6de7ff1c16ccdb008282ade538410f764
ppodds <oscar20020629@gmail.com>
2022-01-08 01:55:29 +0800

    Feat: add comment in video_decoder.cpp

--- AAAAnimation/ffmpeg/video_decoder.cpp
+++ AAAAnimation/ffmpeg/video_decoder.cpp
@@ -4,9 +4,12 @@
 #include <stdio.h>
 
 VideoDecoder::VideoDecoder(std::string video_path) {
+	// allocate memory for format_context
 	format_context = avformat_alloc_context();
+	// get info from video file
 	if (avformat_open_input(&format_context, video_path.c_str(), nullptr, nullptr) != 0)
 		throw std::exception("Encounter error while opening video file!");
+	// wrapper streams and code paramenters with StreamWrapper
 	for (size_t i = 0; i < format_context->nb_streams; i++) {
 		AVStream* in_stream = format_context->streams[i];
 		AVCodecParameters* code_par = in_stream->codecpar;
@@ -15,6 +18,7 @@ VideoDecoder::VideoDecoder(std::string video_path) {
 		else if (code_par->codec_type == AVMEDIA_TYPE_AUDIO)
 			audio = StreamWrapper(in_stream, code_par);
 	}
+	// load decoders
 	video.load_codec_context();
 	audio.load_codec_context();
 	temp_packet = nullptr;
@@ -23,6 +27,7 @@ VideoDecoder::VideoDecoder(std::string video_path) {
 
 VideoDecoder::~VideoDecoder()
 {
+	// release memory
 	avformat_close_input(&format_context);
 	av_packet_free(&temp_packet);
 	av_frame_free(&current_frame);
@@ -31,26 +36,33 @@ VideoDecoder::~VideoDecoder()
 AVFrame* VideoDecoder::next_frame() {
 	if (temp_packet == nullptr)
 	{
+		// allocate memory for temp_packet and current_frame
 		temp_packet = av_packet_alloc();
 		current_frame = av_frame_alloc();
 	}
 	else {
+		// clear content of temp_packet and current_frame
 		av_packet_unref(temp_packet);
 		av_frame_unref(current_frame);
 	}
+	// read file until get a frame
 	while (av_read_frame(format_context, temp_packet) >= 0)
 	{
 		AVCodecContext* codec_context;
+		// get decoder according to stream
 		if (temp_packet->stream_index == video.get_stream()->index)
 			codec_context = video.get_codec_context();
 		else if (temp_packet->stream_index == audio.get_stream()->index)
 			codec_context = audio.get_codec_context();
 		else
 			continue;
+		// senda a packet to the decoder (encoded data)
 		avcodec_send_packet(codec_context, temp_packet);
+		// receive a frame from the decoder (decoded data)
 		avcodec_receive_frame(codec_context, current_frame);
 		return current_frame;
 	}
+	// set current_frame = nullptr if this is EOF
 	current_frame = nullptr;
 	return current_frame;
 }
